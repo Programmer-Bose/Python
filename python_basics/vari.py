da755b26fc246e567e7ac9c41dc15e1a0f27c5f6
Programmer-Bose <basupriyabrata5@gmail.com>
2021-02-14 10:42:49 +0530

    python basic codes

---
+++ python_basics/vari.py
@@ -0,0 +1,16 @@
+x=2
+y=3
+print(x)
+print(y)
+print(x+y)
+#-------------------------------------
+x=9
+y=3
+_=4
+print(x)
+print(y)
+print(x+y)
+print(x+y+_)
+#------------------------------------------
+name='hello'
+print(name)
